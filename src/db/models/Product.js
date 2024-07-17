703c9cffb4897b026ca134ae99762031e0bdd263
AlexxSich <alexxsich@gmail.com>
2024-07-17 20:36:47 -0300

    basic schema

---
+++ src/db/models/Product.js
@@ -0,0 +1,25 @@
+import { model, Schema } from 'mongoose';
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+
+    category: {
+      type: String,
+      enum: ['books', 'electronics', 'clothing', 'other'],
+      default: 'other',
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {},
+);
---
+++ src/server.js
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express from 'express';
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
